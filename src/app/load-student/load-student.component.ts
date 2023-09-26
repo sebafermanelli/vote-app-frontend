f184d9dab62923138b9ce33bfb63053009894fbd
santy <santicinel@gmail.com>
2023-09-26 11:52:57 -0300

    alta alumno finish

--- src/app/load-student/load-student.component.ts
+++ src/app/load-student/load-student.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-load-student',
@@ -7,19 +9,43 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./load-student.component.scss']
 })
 export class LoadStudentComponent {
-loadStudent:FormGroup
+loadStudent:FormGroup;
+modalRef?: BsModalRef;
+message?: string;
+showModal = false;
 
-constructor(private fb:FormBuilder){
+
+constructor(private fb:FormBuilder,private modalService: BsModalService,private router: Router){
 
   this.loadStudent=this.fb.group({
     studentName:['',Validators.required],
     studentLastname:['',Validators.required],
+    studentID:['',Validators.required],
     studentAdress:['',Validators.required],
     studentEmail:['',Validators.required,Validators.email],
     studentPhone:['',Validators.required],
-
-
-
+    studentCourse:['',Validators.required],
+    studentPhoto:['',Validators.required]
   })
+}
+
+
+
+openModal(template: TemplateRef<any>) {
+  this.modalRef = this.modalService.show(template, { class: 'modal-sm' });
+}
+
+confirm(): void {
+  this.showModal = true;
+  this.modalRef?.hide();
+  this.studentOk()
+}
+
+decline(): void {
+  this.showModal = false;
+  this.modalRef?.hide();
+}
+studentOk(){
+  this.router.navigate(['admin'])
 }
 }
