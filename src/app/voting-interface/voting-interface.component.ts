0e234f619b6f6661f957f648f89acee3abdbd52a
EstebanKa <estebankarlenaguirre@gmail.com>
2023-10-04 00:24:23 -0300

    first test with donut graph

--- src/app/voting-interface/voting-interface.component.ts
+++ src/app/voting-interface/voting-interface.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { Chart } from "chart.js/auto";
  
 
 @Component({
@@ -8,12 +9,41 @@ import { Component, OnInit } from "@angular/core";
 
 })
 
+
 export class VotingInterfaceComponent implements OnInit{
 
-    
+    public chart: any;
 
     ngOnInit(): void {
-        
+        this.createChart();
     }
+
+    createChart(){
+
+        this.chart = new Chart("MyChart", {
+          type: 'doughnut', //this denotes tha type of chart
+    
+          data: {// values on X-Axis
+            labels: ['Red', 'Pink','Green','Yellow','Orange','Blue', ],
+               datasets: [{
+        label: 'My First Dataset',
+        data: [600, 240, 100, 432, 253, 34],
+        backgroundColor: [
+          'red',
+          'pink',
+          'green',
+          'yellow',
+          'orange',
+          'blue',			
+        ],
+        hoverOffset: 4
+      }],
+          },
+          options: {
+            aspectRatio:0
+          }
+    
+        });
+      }
       
 }
