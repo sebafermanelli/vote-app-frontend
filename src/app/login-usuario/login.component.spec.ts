4dde76a34ca4fcf7c6b115a44068d6d5d8e485d9
santy <santicinel@gmail.com>
2023-11-07 20:13:13 -0300

    test unitario en login-user

--- src/app/login-usuario/login.component.spec.ts
+++ src/app/login-usuario/login.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LoginComponent } from './login.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -7,7 +8,8 @@ describe('LoginComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [LoginComponent]
+      declarations: [LoginComponent],
+      imports: [RouterTestingModule]
     });
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
@@ -16,5 +18,10 @@ describe('LoginComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should have a valid form control for DNI', () => {
+    const dniControl = component.userForm.get('DNI');
+    expect(dniControl).toBeDefined();
+    
   });
 });
